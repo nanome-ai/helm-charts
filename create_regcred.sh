6cc4a33e6ddfa7f03fbefaa71447481b097cd5c7
Mike Rosengrant <mike.rosengrant@nanome.ai>
2022-06-01 15:15:08 -0400

    update release to point to master

--- create_regcred.sh
+++
@@ -1,5 +0,0 @@
-kubectl delete secret regcred
-kubectl create secret docker-registry regcred  \
---docker-server=441665557124.dkr.ecr.us-west-1.amazonaws.com \
---docker-username=AWS \
---docker-password=$(aws ecr get-login-password)
